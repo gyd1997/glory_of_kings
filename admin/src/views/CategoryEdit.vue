2d317bc26984f0ac74893c06774f66cf02a3dbc9
gyd1997 <gyd0008@163.com>
2020-05-03 22:57:35 +0800

    '子分类'

--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -2,6 +2,15 @@
   <div>
     <h1>{{id ? '编辑': '新建'}}分类</h1>
     <el-form label-width="120px" @submit.native.prevent="save">
+      <el-form-item label="上级分类">
+        <el-select v-model="model.parent">
+          <el-option
+            v-for="item of parents"
+            :key="item._id"
+            :label="item.name"
+            :value="item._id"></el-option>
+        </el-select>
+      </el-form-item>
       <el-form-item label="名称">
         <el-input v-model="model.name"></el-input>
       </el-form-item>
@@ -20,12 +29,13 @@
     },
     data() {
       return {
-        model: {}
+        model: {},
+        parents: []
       }
     },
     methods: {
       async save() {
-        let res
+        let res // eslint-disable-line no-unused-vars
         if(this.id) {
           res = await this.$http.put(`categories/${this.id}`, this.model)
         } else {
@@ -39,11 +49,15 @@
       },
       async fetch() {
         const res = await this.$http.get(`categories/${this.id}`)
-        console.log(res)
         this.model = res.data
+      },
+      async fetchParents() {
+        const res = await this.$http.get(`categories`)
+        this.parents = res.data
       }
     },
     created() {
+      this.fetchParents()
       this.id && this.fetch()
     }
   }
--- admin/src/views/CategoryList.vue
+++ admin/src/views/CategoryList.vue
@@ -3,6 +3,7 @@
     分类列表
     <el-table :data="items">
       <el-table-column prop="_id" label="ID" width="230"></el-table-column>
+      <el-table-column prop="parent.name" label="上级分类"></el-table-column>
       <el-table-column prop="name" label="分类名称"></el-table-column>
       <el-table-column fixed="right" label="操作" width="180">
         <template slot-scope="scope">

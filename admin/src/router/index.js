5b5ae096a0e7cac392a433fe391c0069d8bd64ef
gyd1997 <gyd0008@163.com>
2020-05-06 22:25:09 +0800

    英雄管理

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -7,6 +7,9 @@ import CategoryList from '../views/CategoryList'
 import ItemEdit from '../views/ItemEdit'
 import ItemList from '../views/ItemList'
 
+import HeroEdit from '../views/HeroEdit'
+import HeroList from '../views/HeroList'
+
 Vue.use(VueRouter)
 
   const routes = [
@@ -18,9 +21,14 @@ Vue.use(VueRouter)
       { path: '/categories/create', component: CategoryEdit },
       { path: '/categories/edit/:id', component: CategoryEdit, props: true },
       { path: '/categories/list', component: CategoryList },
+
       { path: '/items/create', component: ItemEdit },
       { path: '/items/edit/:id', component: ItemEdit, props: true },
       { path: '/items/list', component: ItemList },
+
+      { path: '/heroes/create', component: HeroEdit },
+      { path: '/heroes/edit/:id', component: HeroEdit, props: true },
+      { path: '/heroes/list', component: HeroList },
     ]
   },
   
---
+++ admin/src/views/HeroList.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    英雄列表
+    <el-table :data="items">
+      <el-table-column prop="_id" label="ID" width="230"></el-table-column>
+      <el-table-column prop="name" label="英雄名称"></el-table-column>
+      <el-table-column prop="title" label="称号"></el-table-column>
+      <el-table-column prop="avatar" label="图标">
+        <template slot-scope="scope">
+          <img :src="scope.row.avatar" style="height: 3rem;">
+        </template>
+      </el-table-column>
+      <el-table-column fixed="right" label="操作" width="180">
+        <template slot-scope="scope">
+          <el-button type="text" size="small"
+                     @click="$router.push(`/heroes/edit/${scope.row._id}`)">编辑
+          </el-button>
+          <el-button type="text" size="small"
+                     @click="remove(scope.row)">删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ItemsList',
+    data() {
+      return {
+        items: []
+      }
+    },
+    methods: {
+      async fetch() {
+        const res = await this.$http.get('rest/heroes')
+        this.items = res.data
+      },
+      async remove(row) {
+        this.$confirm(`是否确定要删除分类"${row.name}"`, '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'danger'
+        }).then(async () => {
+          await this.$http.delete(`rest/heroes/${row._id}`)
+          this.$message({
+            type: 'success',
+            message: '删除成功！'
+          })
+          this.fetch()
+        })
+
+      }
+    },
+    created() {
+      this.fetch()
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>

cc1cf0874f18387ea0f9a68f48825b281a05afc9
gyd1997 <gyd0008@163.com>
2020-05-07 23:00:50 +0800

    广告位管理

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -13,6 +13,9 @@ import HeroList from '../views/HeroList'
 import ArticleEdit from '../views/ArticleEdit'
 import ArticleList from '../views/ArticleList'
 
+import AdEdit from '../views/AdEdit'
+import AdList from '../views/AdList'
+
 Vue.use(VueRouter)
 
   const routes = [
@@ -36,6 +39,10 @@ Vue.use(VueRouter)
       { path: '/articles/create', component: ArticleEdit },
       { path: '/articles/edit/:id', component: ArticleEdit, props: true },
       { path: '/articles/list', component: ArticleList },
+
+      { path: '/ads/create', component: AdEdit },
+      { path: '/ads/edit/:id', component: AdEdit, props: true },
+      { path: '/ads/list', component: AdList },
     ]
   },
   

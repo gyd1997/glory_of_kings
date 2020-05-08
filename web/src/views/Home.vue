dac88b60419a966dad65bc6784df7edb4609bdbc
gyd1997 <gyd0008@163.com>
2020-05-09 00:50:21 +0800

    精灵图

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -12,6 +12,19 @@
       </swiper-slide>
       <div class="swiper-pagination pagination-home text-right px-3 pb-1" slot="pagination"></div>
     </swiper>
+
+    <div class="nav-icons bg-white mt-3 text-center pt-3 text-dark-1">
+      <div class="d-flex flex-wrap">
+        <div class="nav-item mb-3" v-for="n in 10" :key="n">
+          <i class="sprite sprite-news"></i>
+          <div class="py-2">爆料站</div>
+        </div>
+      </div>
+      <div class="bg-light py-2 fs-sm">
+        <i class="sprite sprite-arrow mr-1"></i>
+        <span>收起</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,6 +36,9 @@ export default {
       swiperOption: {
         pagination: {
           el: ".pagination-home"
+        },
+        autoplay: {
+          delay: 5000
         }
       }
     };
@@ -31,15 +47,26 @@ export default {
 </script>
 
 <style lang="scss">
-@import '../assets/scss/variables.scss';
+@import "../assets/scss/variables.scss";
 
 .pagination-home {
   .swiper-pagination-bullet {
     opacity: 1;
-    border-radius: .1538rem;
-    background: map-get($colors, 'white');
+    border-radius: 0.1538rem;
+    background: map-get($colors, "white");
     &.swiper-pagination-bullet-active {
-      background: map-get($colors, 'info');
+      background: map-get($colors, "info");
+    }
+  }
+}
+.nav-icons {
+  border-top: 1px solid $border-color;
+  border-bottom: 1px solid $border-color;
+  .nav-item {
+    width: 25%;
+    border-right: 1px solid $border-color;
+    &:nth-child(4n) {
+      border-right: none;
     }
   }
 }

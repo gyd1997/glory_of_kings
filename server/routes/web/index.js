d6081ad62639f6f6c437c799e0dc514ca9808879
gyd1997 <gyd0008@163.com>
2020-05-19 22:40:04 +0800

    新闻资讯数据接口

--- server/routes/web/index.js
+++ server/routes/web/index.js
@@ -43,8 +43,58 @@ module.exports = app => {
       }
     })
     await Article.deleteMany({})
-    await Article.insertMany({newsList})
+    await Article.insertMany({ newsList })
     res.send(newsList)
+  })
+
+  router.get('/news/list', async (req, res) => {
+    /* const parent = await Category.findOne({
+      name: '新闻分类'
+    }).populate({
+      path: 'children',
+      populate: {
+        path: 'newsList'
+      }
+    }).lean() */
+    const parent = await Category.findOne({
+      name: '新闻分类',
+    })
+    const cats = await Category.aggregate([
+      { $match: { parent: parent._id } },
+      {
+        $lookup: {
+          from: 'articles',
+          localField: '_id',
+          foreignField: 'categories',
+          as: 'newsList',
+        },
+      },
+      {
+        $addFields: {
+          newsList: { $slice: ['$newsList', 5] },
+        },
+      },
+    ])
+    const subCats = cats.map(v => v._id)
+    cats.unshift({
+      name: '热门',
+      newsList: await Article.find()
+        .where({
+          categories: { $in: subCats },
+        })
+        .populate('categories')
+        .limit(5)
+        .lean(),
+    })
+    cats.map(cat => {
+      cat.newsList.map(news => {
+        news.categoryName =
+          cat.name === '热门' ? news.categories[0].name : cat.name
+        return news
+      })
+      return cat
+    })
+    res.send(cats)
   })
 
   app.use('/web/api', router)
